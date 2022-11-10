9dbbf6a1022d76486de9da41b2498a6b8f33595b
VianaSamuel <samuluizzz@hotmail.com>
2022-11-10 16:58:59 -0300

    Adição de JS.

---
+++ etc/bazar.js
@@ -0,0 +1,4 @@
+function toggleContent() {
+    const contentId = document.getElementById("filtro");
+    contentId.style.display == "flex" ? contentId.style.display = "none" : contentId.style.display = "flex";
+  }
